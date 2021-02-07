1078dd0b4c90ffd1c2a1ef3b07548011471d16bc
elyselam <elyse.p.lam@gmail.com>
2021-02-06 16:00:47 -0800

    added hero detail

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  //hero property must be Input property bc external HeroesComp html will bind to it
+  @Input() hero: Hero;
+
+}
