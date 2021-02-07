a098720bd27fdebdb93cfee343fdc23bd47154d1
elyselam <elyse.p.lam@gmail.com>
2021-02-06 20:07:26 -0800

    adding message service, message component next

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import {HEROES} from './mock-heroes';
 import {Hero} from './hero';
 import {Observable, of} from 'rxjs';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    //notifies when heroes are fetched
+    this.messageService.add('HeroService: fetched heroes');
     //of() returns Observable<Hero[]> that emits a single value, the array
     return of(HEROES);
   }
